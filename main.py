79b644a4fe30374d344164b8a8a4a18148e12104
nickdeanolson-dev <nick.dean.olson@gmail.com>
2026-07-10 23:57:24 -0700

    up to lesson 3.5

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from logger import log_state
 from player import *
 
 
+
+
 def main():
     print("Hello from asteroids!")
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -13,6 +15,13 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0.0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    # Player is the name of the class, not an instance of it
+    # This must be done before any Player objects are created
+    Player.containers = (updatable, drawable)
+
+
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     while True:
         log_state()
@@ -20,7 +29,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.draw(screen)
+        
+        updatable.update(dt)
+        for item in drawable:
+            item.draw(screen)
+        
         dt = clock.tick(60)/1000
         pygame.display.flip()
 
