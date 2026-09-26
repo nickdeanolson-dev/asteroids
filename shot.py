ed27f97242ad98d4d993844b3de73f1068526c75
nickdeanolson-dev <nick.dean.olson@gmail.com>
2026-09-25 19:33:29 -0700

    Add shot range limit

--- shot.py
+++ shot.py
@@ -6,7 +6,7 @@ from constants import *
 class Shot(CircleShape):
     def __init__(self,x,y):
         super().__init__(x,y,SHOT_RADIUS)
-
+        self.lifetime = 0.0
 
     def draw(self, screen):
         pygame.draw.circle(screen, "white", self.position, self.radius, LINE_WIDTH)
@@ -14,3 +14,6 @@ class Shot(CircleShape):
     def update(self, dt):
         self.position += self.velocity*dt
         self.wrap_position()
+        self.lifetime += dt
+        if self.lifetime >= SHOT_LIFETIME_SECONDS:
+            self.kill()
